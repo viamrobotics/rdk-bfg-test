e90bf4b0f5979ad345de519189eeee1a827fbdec
Peter LoVerso <biotinker@gmail.com>
2021-06-29 14:22:13 -0700

    Separate math-specific functions from kinematics (#122)
    * Move kinematics/kinmath to spatialmath
    
    * Rename QuatTrans to DualQuaternion, split out OrientationVector
    
    Former-commit-id: dd6068222126f10f47544700af24b3e87ec2d4cf

---
+++ spatialmath/dualquaternion.go
@@ -0,0 +1,315 @@
+// Package spatialmath defines spatial mathematical operations
+package spatialmath
+
+import (
+	"math"
+
+	pb "go.viam.com/core/proto/api/v1"
+	"go.viam.com/core/utils"
+
+	"github.com/go-gl/mathgl/mgl64"
+	"gonum.org/v1/gonum/num/dualquat"
+	"gonum.org/v1/gonum/num/quat"
+)
+
+const radToDeg = 180 / math.Pi
+const degToRad = math.Pi / 180
+
+// If two angles differ by less than this amount, we consider them the same for the purpose of doing
+// math around the poles of orientation.
+const angleEpsilon = 0.01 // radians
+
+// DualQuaternion defines functions to perform rigid DualQuaternionformations in 3D.
+type DualQuaternion struct {
+	Quat dualquat.Number
+}
+
+// NewDualQuaternion returns a pointer to a new DualQuaternion object whose Quaternion is an identity Quaternion.
+// Since the real part of a qual quaternion should be a unit quaternion, not all zeroes, this should be used
+// instead of &DualQuaternion{}.
+func NewDualQuaternion() *DualQuaternion {
+	return &DualQuaternion{dualquat.Number{
+		Real: quat.Number{Real: 1},
+		Dual: quat.Number{},
+	}}
+}
+
+// NewDualQuaternionFromRotation returns a pointer to a new DualQuaternion object whose rotation quaternion is set from a provided
+// orientation vector.
+func NewDualQuaternionFromRotation(ov *OrientationVec) *DualQuaternion {
+	// Handle the zero case
+	if ov.OX == 0 && ov.OY == 0 && ov.OZ == 0 {
+		ov.OX = 1
+	}
+	NormalizeOV(ov)
+	return &DualQuaternion{dualquat.Number{
+		Real: OVToQuat(ov),
+		Dual: quat.Number{},
+	}}
+}
+
+// NewDualQuaternionFromArmPos returns a pointer to a new DualQuaternion object whose rotation quaternion is set from a provided
+// arm position.
+func NewDualQuaternionFromArmPos(pos *pb.ArmPosition) *DualQuaternion {
+	q := NewDualQuaternionFromRotation(&OrientationVec{pos.Theta * degToRad, pos.OX, pos.OY, pos.OZ})
+	q.SetTranslation(pos.X, pos.Y, pos.Z)
+	return q
+}
+
+// ToArmPos converts a DualQuaternion to
+func (q *DualQuaternion) ToArmPos() *pb.ArmPosition {
+	final := &pb.ArmPosition{}
+	cartQuat := dualquat.Mul(q.Quat, dualquat.Conj(q.Quat))
+	final.X = cartQuat.Dual.Imag
+	final.Y = cartQuat.Dual.Jmag
+	final.Z = cartQuat.Dual.Kmag
+	poseOV := QuatToOV(q.Quat.Real)
+	final.Theta = utils.RadToDeg(poseOV.Theta)
+	final.OX = poseOV.OX
+	final.OY = poseOV.OY
+	final.OZ = poseOV.OZ
+	return final
+}
+
+// Clone returns a DualQuaternion object identical to this one.
+func (q *DualQuaternion) Clone() *DualQuaternion {
+	t := &DualQuaternion{}
+	// No need for deep copies here, dualquats are primitives all the way down
+	t.Quat = q.Quat
+	return t
+}
+
+// Rotation returns the rotation quaternion.
+func (q *DualQuaternion) Rotation() quat.Number {
+	return q.Quat.Real
+}
+
+// Translation multiplies the dual quaternion by its own conjugate to give a dq where the real is the identity quat,
+// and the dual is representative of 0.5 * real world millimeters.
+func (q *DualQuaternion) Translation() dualquat.Number {
+	return dualquat.Mul(q.Quat, dualquat.Conj(q.Quat))
+}
+
+// SetTranslation correctly sets the translation quaternion against the rotation.
+func (q *DualQuaternion) SetTranslation(x, y, z float64) {
+	q.Quat.Dual = quat.Number{0, x / 2, y / 2, z / 2}
+	q.Rotate()
+}
+
+// SetX sets the x translation.
+func (q *DualQuaternion) SetX(x float64) {
+	q.Quat.Dual.Imag = x
+}
+
+// SetY sets the y translation.
+func (q *DualQuaternion) SetY(y float64) {
+	q.Quat.Dual.Jmag = y
+}
+
+// SetZ sets the z translation.
+func (q *DualQuaternion) SetZ(z float64) {
+	q.Quat.Dual.Kmag = z
+}
+
+// Rotate multiplies the dual part of the quaternion by the real part give the correct rotation.
+func (q *DualQuaternion) Rotate() {
+	q.Quat.Dual = quat.Mul(q.Quat.Dual, q.Quat.Real)
+}
+
+// ToDelta returns the difference between two DualQuaternion'.
+// We use quaternion/angle axis for this because distances are well-defined.
+func (q *DualQuaternion) ToDelta(other *DualQuaternion) []float64 {
+	ret := make([]float64, 7)
+
+	quatBetween := quat.Mul(other.Quat.Real, quat.Conj(q.Quat.Real))
+
+	otherTrans := dualquat.Mul(other.Quat, dualquat.Conj(other.Quat))
+	mTrans := dualquat.Mul(q.Quat, dualquat.Conj(q.Quat))
+	aa := QuatToR4AA(quatBetween)
+	ret[0] = otherTrans.Dual.Imag - mTrans.Dual.Imag
+	ret[1] = otherTrans.Dual.Jmag - mTrans.Dual.Jmag
+	ret[2] = otherTrans.Dual.Kmag - mTrans.Dual.Kmag
+	ret[3] = aa.Theta
+	ret[4] = aa.RX
+	ret[5] = aa.RY
+	ret[6] = aa.RZ
+	return ret
+}
+
+// ToDeltaR3 returns the difference between two DualQuaternion' using R3 angle axis.
+// We use quaternion/angle axis for this because distances are well-defined.
+func (q *DualQuaternion) ToDeltaR3(other *DualQuaternion) []float64 {
+	ret := make([]float64, 6)
+
+	quatBetween := quat.Mul(other.Quat.Real, quat.Conj(q.Quat.Real))
+
+	otherTrans := dualquat.Mul(other.Quat, dualquat.Conj(other.Quat))
+	mTrans := dualquat.Mul(q.Quat, dualquat.Conj(q.Quat))
+	aa := QuatToR3AA(quatBetween)
+	ret[0] = otherTrans.Dual.Imag - mTrans.Dual.Imag
+	ret[1] = otherTrans.Dual.Jmag - mTrans.Dual.Jmag
+	ret[2] = otherTrans.Dual.Kmag - mTrans.Dual.Kmag
+	ret[3] = aa.RX
+	ret[4] = aa.RY
+	ret[5] = aa.RZ
+	return ret
+}
+
+// Transformation multiplies the dual quat contained in this DualQuaternion by another dual quat.
+func (q *DualQuaternion) Transformation(by dualquat.Number) dualquat.Number {
+	// Ensure we are multiplying by a unit dual quaternion
+	if vecLen := quat.Abs(by.Real); vecLen != 1 {
+		by.Real = quat.Scale(1/vecLen, by.Real)
+	}
+
+	return dualquat.Mul(q.Quat, by)
+}
+
+// QuatToR4AA converts a quat to an R4 axis angle in the same way the C++ Eigen library does.
+// https://eigen.tuxfamily.org/dox/AngleAxis_8h_source.html
+func QuatToR4AA(q quat.Number) R4AA {
+	denom := Norm(q)
+
+	angle := 2 * math.Atan2(denom, math.Abs(q.Real))
+	if q.Real < 0 {
+		angle *= -1
+	}
+
+	if denom < 1e-6 {
+		return R4AA{angle, 1, 0, 0}
+	}
+	return R4AA{angle, q.Imag / denom, q.Jmag / denom, q.Kmag / denom}
+}
+
+// QuatToR3AA converts a quat to an R3 axis angle in the same way the C++ Eigen library does.
+// https://eigen.tuxfamily.org/dox/AngleAxis_8h_source.html
+func QuatToR3AA(q quat.Number) R3AA {
+	denom := Norm(q)
+
+	angle := 2 * math.Atan2(denom, math.Abs(q.Real))
+	if q.Real < 0 {
+		angle *= -1
+	}
+
+	if denom < 1e-6 {
+		return R3AA{1, 0, 0}
+	}
+	return R3AA{angle * q.Imag / denom, angle * q.Jmag / denom, angle * q.Kmag / denom}
+}
+
+// QuatToEuler converts a rotation unit quaternion to euler angles.
+// See the following wikipedia page for the formulas used here:
+// https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles#Quaternion_to_Euler_angles_conversion
+// Euler angles are terrible, don't use them.
+func QuatToEuler(q quat.Number) []float64 {
+	w := q.Real
+	x := q.Imag
+	y := q.Jmag
+	z := q.Kmag
+
+	var angles []float64
+
+	angles = append(angles, math.Atan2(2*(w*x+y*z), 1-2*(x*x+y*y)))
+	angles = append(angles, math.Asin(2*(w*y-x*z)))
+	angles = append(angles, math.Atan2(2*(w*z+y*x), 1-2*(y*y+z*z)))
+
+	for i := range angles {
+
+		angles[i] *= radToDeg
+	}
+	return angles
+}
+
+// QuatToOV converts a quaternion to an orientation vector
+func QuatToOV(q quat.Number) *OrientationVec {
+	xAxis := quat.Number{0, 1, 0, 0}
+	zAxis := quat.Number{0, 0, 0, 1}
+	ov := &OrientationVec{}
+	// Get the xyz point of our axis on the unit sphere
+	xyz := quat.Mul(quat.Mul(q, xAxis), quat.Conj(q))
+	newZ := quat.Mul(quat.Mul(q, zAxis), quat.Conj(q))
+	ov.OX = xyz.Imag
+	ov.OY = xyz.Jmag
+	ov.OZ = xyz.Kmag
+
+	// The contents of ov.xyz.Kmag are not in radians but we use angleEpsilon anyway to check how close we are to the pole
+	if 1-math.Abs(xyz.Kmag) < angleEpsilon {
+		// Special case for when we point directly along the Z axis
+		// Get the vector normal to the local-x, global-z, origin plane
+		ov.Theta = math.Atan2(newZ.Jmag, newZ.Imag)
+		if xyz.Kmag > 0 {
+			ov.Theta = math.Atan2(newZ.Jmag, -newZ.Imag)
+		}
+
+	} else {
+		v1 := mgl64.Vec3{xyz.Imag, xyz.Jmag, xyz.Kmag}
+		v2 := mgl64.Vec3{zAxis.Imag, zAxis.Jmag, zAxis.Kmag}
+		norm1 := v1.Cross(v2)
+
+		// Get the vector normal to the local-x, local-z, origin plane
+		norm2 := v1.Cross(mgl64.Vec3{newZ.Imag, newZ.Jmag, newZ.Kmag})
+
+		// For theta, we find the angle between the plane defined by local-x, global-z, origin and local-x, local-z, origin
+		cosTheta := norm1.Dot(norm2) / (norm1.Len() * norm2.Len())
+		// Account for floating point error
+		if cosTheta > 1 {
+			cosTheta = 1
+		}
+		if cosTheta < -1 {
+			cosTheta = -1
+		}
+
+		theta := math.Acos(cosTheta)
+		if theta > angleEpsilon {
+			// Acos will always produce a positive number, we need to determine directionality of the angle
+			// We rotate newZ by -theta around the xyz axis and see if we wind up coplanar with local-x, global-z, origin
+			// If so theta is positive, otherwise negative
+			// An R4AA is a convenient way to rotate a point by an amount around an arbitrary axis
+			aa := R4AA{-theta, xyz.Imag, xyz.Jmag, xyz.Kmag}
+			q2 := aa.ToQuat()
+			testZ := quat.Mul(quat.Mul(q2, newZ), quat.Conj(q2))
+			norm3 := v1.Cross(mgl64.Vec3{testZ.Imag, testZ.Jmag, testZ.Kmag})
+			cosTest := norm1.Dot(norm3) / (norm1.Len() * norm3.Len())
+			if 1-cosTest < angleEpsilon {
+				ov.Theta = theta
+			} else {
+				ov.Theta = -theta
+			}
+		} else {
+			ov.Theta = 0
+		}
+	}
+
+	return ov
+}
+
+// MatToEuler Converts a 4x4 matrix to Euler angles.
+// Euler angles are terrible, don't use them.
+func MatToEuler(mat mgl64.Mat4) []float64 {
+	sy := math.Sqrt(mat.At(0, 0)*mat.At(0, 0) + mat.At(1, 0)*mat.At(1, 0))
+	singular := sy < 1e-6
+	var angles []float64
+	if singular {
+		angles = append(angles, math.Atan2(-mat.At(1, 2), mat.At(1, 1)))
+		angles = append(angles, math.Atan2(-mat.At(2, 0), sy))
+		angles = append(angles, 0)
+	} else {
+		angles = append(angles, math.Atan2(mat.At(2, 1), mat.At(2, 2)))
+		angles = append(angles, math.Atan2(-mat.At(2, 0), sy))
+		angles = append(angles, math.Atan2(mat.At(1, 0), mat.At(0, 0)))
+	}
+	for i := range angles {
+		angles[i] *= radToDeg
+	}
+	return angles
+}
+
+// Norm returns the norm of the quaternion, i.e. the sqrt of the squares of the imaginary parts.
+func Norm(q quat.Number) float64 {
+	return math.Sqrt(q.Imag*q.Imag + q.Jmag*q.Jmag + q.Kmag*q.Kmag)
+}
+
+// Flip will multiply a quaternion by -1, returning a quaternion representing the same orientation but in the opposing octant.
+func Flip(q quat.Number) quat.Number {
+	return quat.Number{-q.Real, -q.Imag, -q.Jmag, -q.Kmag}
+}
---
+++ spatialmath/quat_test.go
@@ -0,0 +1,154 @@
+package spatialmath
+
+import (
+	"math"
+	"testing"
+
+	"go.viam.com/test"
+
+	"gonum.org/v1/gonum/num/quat"
+)
+
+func TestAngleAxisConversion1(t *testing.T) {
+	// Test that we can convert back and forth losslessly between angle axis and quaternions
+
+	startAA := R4AA{2.5980762, 0.577350, 0.577350, 0.577350}
+	quat := startAA.ToQuat()
+	end1 := QuatToR4AA(quat)
+	test.That(t, math.Abs(end1.Theta-startAA.Theta), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(end1.RX-startAA.RX), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(end1.RY-startAA.RZ), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(end1.RZ-startAA.RZ), test.ShouldBeLessThan, 0.001)
+}
+
+func TestAngleAxisConversion2(t *testing.T) {
+	// Test that we can convert back and forth losslessly between r4 and r3 angle axis
+
+	startAA := R4AA{2.5980762, 0.577350, 0.577350, 0.577350}
+	r3 := startAA.ToR3()
+	end1 := r3.ToR4()
+	test.That(t, math.Abs(end1.Theta-startAA.Theta), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(end1.RX-startAA.RX), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(end1.RY-startAA.RZ), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(end1.RZ-startAA.RZ), test.ShouldBeLessThan, 0.001)
+}
+
+func TestFlip(t *testing.T) {
+	// Test that flipping quaternions to the opposite octant results in the same rotation
+
+	startAA := R4AA{2.5980762, 0.577350, -0.577350, -0.577350}
+	quat1 := startAA.ToQuat()
+	quat2 := startAA.ToQuat()
+	qb1 := quat.Mul(quat1, quat.Conj(quat2))
+	qb2 := quat.Mul(quat1, quat.Conj(Flip(quat2)))
+
+	end1 := QuatToR4AA(qb1)
+	end2 := QuatToR4AA(qb2)
+	test.That(t, math.Abs(end1.Theta-end2.Theta), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(end1.RX-end2.RX), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(end1.RY-end2.RZ), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(end1.RZ-end2.RZ), test.ShouldBeLessThan, 0.001)
+}
+
+func TestOVConversion(t *testing.T) {
+	// Ensure a robust, lossless quaternion/ov/quaternion/ov transformation
+	q1 := quat.Number{0.96, -0.28, 0, 0}
+	ov1 := QuatToOV(q1)
+	q2 := OVToQuat(ov1)
+	ov2 := QuatToOV(q2)
+	test.That(t, math.Abs(ov1.Theta-ov2.Theta), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(ov1.OX-ov2.OX), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(ov1.OY-ov2.OY), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(ov1.OZ-ov2.OZ), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(q1.Real-q2.Real), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(q1.Imag-q2.Imag), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(q1.Jmag-q2.Jmag), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(q1.Kmag-q2.Kmag), test.ShouldBeLessThan, 0.001)
+
+	// Test the complementary angle
+	q1 = quat.Number{0.96, 0.28, 0, 0}
+	ov1 = QuatToOV(q1)
+	q2 = OVToQuat(ov1)
+	ov2 = QuatToOV(q2)
+	test.That(t, math.Abs(ov1.Theta-ov2.Theta), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(ov1.OX-ov2.OX), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(ov1.OY-ov2.OY), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(ov1.OZ-ov2.OZ), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(q1.Real-q2.Real), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(q1.Imag-q2.Imag), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(q1.Jmag-q2.Jmag), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(q1.Kmag-q2.Kmag), test.ShouldBeLessThan, 0.001)
+
+	// Another odd angle
+	q1 = quat.Number{0.5, -0.5, -0.5, -0.5}
+	ov1 = QuatToOV(q1)
+	q2 = OVToQuat(ov1)
+	ov2 = QuatToOV(q2)
+	test.That(t, math.Abs(ov1.Theta-ov2.Theta), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(ov1.OX-ov2.OX), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(ov1.OY-ov2.OY), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(ov1.OZ-ov2.OZ), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(q1.Real-q2.Real), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(q1.Imag-q2.Imag), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(q1.Jmag-q2.Jmag), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(q1.Kmag-q2.Kmag), test.ShouldBeLessThan, 0.001)
+}
+
+func TestOVConversionPoles(t *testing.T) {
+	// Ensure a robust, lossless quaternion/ov/quaternion/ov transformation near the poles
+	// North pole
+	ov1 := &OrientationVec{Theta: 2.47208, OX: 0, OY: 0, OZ: 1}
+	q1 := OVToQuat(ov1)
+	ov2 := QuatToOV(q1)
+	q2 := OVToQuat(ov2)
+
+	test.That(t, math.Abs(ov1.Theta-ov2.Theta), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(ov1.OX-ov2.OX), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(ov1.OY-ov2.OY), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(ov1.OZ-ov2.OZ), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(q1.Real-q2.Real), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(q1.Imag-q2.Imag), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(q1.Jmag-q2.Jmag), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(q1.Kmag-q2.Kmag), test.ShouldBeLessThan, 0.001)
+
+	// South pole
+	ov1 = &OrientationVec{Theta: 2.47208, OX: 0, OY: 0, OZ: -1}
+	q1 = OVToQuat(ov1)
+	ov2 = QuatToOV(q1)
+	q2 = OVToQuat(ov2)
+
+	test.That(t, math.Abs(ov1.Theta-ov2.Theta), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(ov1.OX-ov2.OX), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(ov1.OY-ov2.OY), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(ov1.OZ-ov2.OZ), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(q1.Real-q2.Real), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(q1.Imag-q2.Imag), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(q1.Jmag-q2.Jmag), test.ShouldBeLessThan, 0.001)
+	test.That(t, math.Abs(q1.Kmag-q2.Kmag), test.ShouldBeLessThan, 0.001)
+}
+
+func TestR4Normalize(t *testing.T) {
+	// Test that Normalize() will produce a unit vector
+	ov1 := R4AA{0, 999, 0, 0}
+	ov1.Normalize()
+	test.That(t, ov1.Theta, test.ShouldEqual, 0)
+	test.That(t, ov1.RX, test.ShouldEqual, 1)
+	test.That(t, ov1.RY, test.ShouldEqual, 0)
+	test.That(t, ov1.RZ, test.ShouldEqual, 0)
+}
+
+func TestOVNormalize(t *testing.T) {
+	// Test that Normalize() will produce a unit vector
+	ov1 := &OrientationVec{Theta: 0, OX: 999, OY: 0, OZ: 0}
+	NormalizeOV(ov1)
+	test.That(t, ov1.Theta, test.ShouldEqual, 0)
+	test.That(t, ov1.OX, test.ShouldEqual, 1)
+	test.That(t, ov1.OY, test.ShouldEqual, 0)
+	test.That(t, ov1.OZ, test.ShouldEqual, 0)
+	ov1 = &OrientationVec{Theta: 0, OX: 0.5, OY: 0, OZ: 0}
+	NormalizeOV(ov1)
+	test.That(t, ov1.Theta, test.ShouldEqual, 0)
+	test.That(t, ov1.OX, test.ShouldEqual, 1)
+	test.That(t, ov1.OY, test.ShouldEqual, 0)
+	test.That(t, ov1.OZ, test.ShouldEqual, 0)
+}
