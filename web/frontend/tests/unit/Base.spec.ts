f2306070f5edd99b1fb3d944d724b572f7823638
Micheal Parks <103450731+micheal-parks@users.noreply.github.com>
2022-07-20 16:04:43 -0400

    Refactor frontend code hackday PR (#991)
    * create app.vue file
    
    * add vite
    
    * delete unused files
    
    * fix build step
    
    * refactor control.js
    
    * fix some js imports
    
    * add prime package and necessary config
    
    * add rollup conversion of proto commonjs to esm
    
    * rollup
    
    * Micheal Re-hack-tor
    
    * Micheal Re-hack-tor
    
    * deleteeeed
    
    * add it back
    
    * add compiled file
    
    * replaces badge from motor detail
    
    * removes container component from rdk
    
    * remove unused files
    
    * add tailwind 3
    
    * icon component
    
    * icon component
    
    * restore root package.json
    
    * add custom element rules to vite
    
    * replace elements with prime element, update prime to latest
    
    * convert remaining collapse and button elements in app.vue to prime elements
    
    * fix templating issue in app.vue
    
    * update eslint config, fix proto imports, and update gamepad to vue 3
    
    * remove grid component
    
    * convert slam to vue3
    
    * migrate info-button to vue3
    
    * convert servo to vue3
    
    * upgrade camera to vue3
    
    * upgrade popper to vue3
    
    * lint rc folder
    
    * lint src/app.vue
    
    * lint input-controller
    
    * lint gamepad
    
    * lint camera and slam
    
    * lint, remove app directive
    
    * keyboard component in progress
    
    * convert base to vue3
    
    * remove unused components, use prime elements in camera
    
    * remove radio buttons
    
    * convert keyboardinput to vue3
    
    * fix keyboard outside click, convert inputs to v-input
    
    * convert motordetail to vue3
    
    * delete unused components
    
    * fix js errors
    
    * add rollup to deps
    
    * delete unused files, fix styling for camera
    
    * delete base.vue
    
    * readd base.vue
    
    * remove camera.vue
    
    * readd camera.vue
    
    * delete dls
    
    * remove slam.vue
    
    * add slam.vue
    
    * remove servo.vue
    
    * add servo.vue
    
    * convert input-number to prime element
    
    * replace range with v-slider
    
    * better filtering for rdk components in app.vue
    
    * fix camera ts errors
    
    * fix more camera typescript errors
    
    * update styles and fix bugs
    
    * servo component
    
    * APP-380 - incorps pwm funcs in control helpers
    
    * APP-380 - adds pwm and pwm freq to board on webappidx branch
    
    * APP-380 - removes comment
    
    * APP-380 - updates pwm inputs to v-model
    
    * APP-380 - update methods to shorthand syntax
    
    * APP-380 - add toast notifications to gpio get methods
    
    * APP-380 - add tailwind height to pwm state option select
    
    * APP-380 - remove use of unneeded ids
    
    * APP-380 - use label in v-input in GPIO
    
    * update prime
    
    * add icon support with prime
    
    * readd servo
    
    * readd third-party for now
    
    * export servocontrolhelper
    
    * fix maps bug
    
    * update Makefile for rollup
    
    * add missing icons, imu bugfixes
    
    * add missing icons, imu bugfixes
    
    * icon and css fixes
    
    * add testing command
    
    * generate buf files for linting
    
    * lint
    
    * host css files and add built assets
    
    * update eslint ignore rules
    
    * update prime
    
    * build assets
    
    * slider fixes
    
    * layout fixes, fix keyboard layout
    
    * fix layout bugs and add changes to webappindex from main
    
    * run build
    
    * layout fixes, prevent dom thrashing when viewing current ops
    
    * remove control mapfile
    
    * readd control file
    
    * run build
    
    * readd window.connect
    
    * run git diff to see source of control.js differences
    
    * run make build lint
    
    * run git diff in ci
    
    * remove git diff
    
    * fix arm bug
    
    * remove eslint from main step
    
    * add git diff
    
    * add debug commands
    
    * build on linux
    
    * remove debugging commands
    
    * rever proto version
    
    * combine prepare and rollup script
    
    Co-authored-by: mrloureed <mrloureed@gmail.com>
    Co-authored-by: Steven Balasta <sbal13@gmail.com>
    Co-authored-by: Anjali Nainani <anjali.nainani@gmail.com>
    Co-authored-by: Anjali Nainani <83609116+anjinai@users.noreply.github.com>
    Co-authored-by: Steve Briskin <steve@viam.com>
    Co-authored-by: Maxim Pertsov <maxim@viam.com>

--- web/frontend/tests/unit/Base.spec.ts
+++ web/frontend/tests/unit/Base.spec.ts
@@ -1,17 +1,7 @@
 import { enableAutoDestroy, mount } from "@vue/test-utils";
 import Base from "@/components/Base.vue";
-import ViamSelect from "@/components/ViamSelect.vue";
-import ViamInput from "@/components/ViamInput.vue";
-import Collapse from "@/components/Collapse.vue";
-import Breadcrumbs from "@/components/Breadcrumbs.vue";
 import ViamIcon from "@/components/ViamIcon.vue";
-import Tabs from "@/components/Tabs.vue";
-import Tab from "@/components/Tab.vue";
-import RadioButtons from "@/components/RadioButtons.vue";
-import ViamButton from "@/components/Button.vue";
 import KeyboardInput from "@/components/KeyboardInput.vue";
-import Range from "@/components/Range.vue";
-import ClickOutside from "@/directives/clickOutside";
 
 describe("Base", () => {
   enableAutoDestroy(afterEach);
@@ -24,20 +14,8 @@ describe("Base", () => {
         crumbs: ["Keyboard", "Discrete"],
       },
       components: {
-        Collapse,
-        Breadcrumbs,
         ViamIcon,
-        RadioButtons,
-        Tabs,
-        Tab,
-        ViamButton,
         KeyboardInput,
-        ViamSelect,
-        ViamInput,
-        Range,
-      },
-      directives: {
-        ClickOutside,
       },
     });
 
@@ -52,17 +30,8 @@ describe("Base", () => {
         crumbs: ["Keyboard", "Discrete"],
       },
       components: {
-        Collapse,
-        Breadcrumbs,
         ViamIcon,
-        RadioButtons,
-        Tabs,
-        Tab,
-        ViamButton,
         KeyboardInput,
-        ViamSelect,
-        ViamInput,
-        Range,
       },
     });
     const firstButton = wrapper.find(".tabs-container button:first-child");
@@ -92,17 +61,8 @@ describe("Base", () => {
         crumbs: ["Keyboard", "Discrete"],
       },
       components: {
-        Collapse,
-        Breadcrumbs,
         ViamIcon,
-        RadioButtons,
-        Tabs,
-        Tab,
-        ViamButton,
         KeyboardInput,
-        ViamSelect,
-        ViamInput,
-        Range,
       },
     });
     const lastButton = wrapper.find(".tabs-container button:last-child");
@@ -126,17 +86,8 @@ describe("Base", () => {
         crumbs: ["Keyboard", "Discrete"],
       },
       components: {
-        Collapse,
-        Breadcrumbs,
         ViamIcon,
-        RadioButtons,
-        Tabs,
-        Tab,
-        ViamButton,
         KeyboardInput,
-        ViamSelect,
-        ViamInput,
-        Range,
       },
     });
     const lastButton = wrapper.find(".tabs-container button:last-child");
@@ -161,17 +112,8 @@ describe("Base", () => {
         crumbs: ["Keyboard", "Discrete"],
       },
       components: {
-        Collapse,
-        Breadcrumbs,
         ViamIcon,
-        RadioButtons,
-        Tabs,
-        Tab,
-        ViamButton,
         KeyboardInput,
-        ViamSelect,
-        ViamInput,
-        Range,
       },
     });
     const lastButton = wrapper.find(".tabs-container button:last-child");
